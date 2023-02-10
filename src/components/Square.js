bd8289015cd587a7aaa57e91de6fe053891a8c01
Ella R <ryukhinaella@gmail.com>
2023-02-09 20:19:16 -0800

    Styled Square component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,15 @@
+
+const style = {
+    background: 'lightblue',
+    border: '2px solid darkblue',
+    fontSize: '30px',
+    fontWeight: '800',
+    cursor: 'pointer',
+    outline: 'none'
+}
+
 const Square = ({ onClick, value }) => (
-    <button onClick={onClick}>
+    <button style={style} onClick={onClick}>
         {value}
     </button>
 );
